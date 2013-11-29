b649974b549012c6f43a237bf4d7e257c15dffb6
Louis Taylor <kragniz@gmail.com>
2013-11-29 01:58:06 +0000

    Add i2c reading function

--- CMPS10/CMPS10.cpp
+++ CMPS10/CMPS10.cpp
@@ -1,5 +1,14 @@
 #include "Arduino.h"
+#include "Wire.h"
 #include "CMPS10.h"
+
+byte _read_i2c(int address, int _register) {
+  Wire.beginTransmission(address);
+  Wire.write(_register);
+  Wire.endTransmission();
+  Wire.requestFrom(address, 1);
+  return Wire.read();
+}
 
 CMPS10::CMPS10() {
     compass_address = 0x60;
